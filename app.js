67451c5570ae62e519e4e974acbc16855b41bb41
esitecch <esitecch@gmail.com>
2019-08-12 10:57:23 -0500

    Actualización de dependencias

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
-var express = require("express"),
-    app = express(),
+'use strict'
+
+var express = require("express"), // Cargamos el módulo de express
+    bodyParser  = require("body-parser"), // Cargamos el módulo de body-parser
+    app = express(), // Llamamos a express para poder crear el servidor
     http = require("http"),
     server = http.createServer(app),
-    bodyParser  = require("body-parser"),
-    methodOverride = require("method-override"),
-    mongoose = require('mongoose');
 
+    methodOverride = require("method-override"),
+    mongoose = require('mongoose'),
+    port = 3000;
+
+//Configuramos bodyParser para que convierta el body de nuestras peticiones a JSON
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-   res.send("Hello World! si funciona");
+router.get('/hola', function(req, res) {
+   res.status(200).send("API Rest for React App");
 });
 
 app.use(router);
-mongodb://username:password@host:port/database
-mongoose.connect('mongodb://root:ak123456@alexkbza.com:27017/bdd-react-app', function(err, res) {
-    if(err) {
-        console.log('ERROR: connecting to Database. ' + err);
-    }
-    app.listen(3000, function() {
-        console.log("Node server running on http://localhost:3000 - si funciona la conexion");
-    });
-});
+/*'mongodb://root:ak123456@alexkbza.com:27017/bdd-react-app',*/
+/*'mongodb://root:lmery1997@merylema.com:27017/ProjectGyA'*/
+mongoose.connect('mongodb://root:ak123456@alexkbza.com:27017/bdd-react-app', { useMongoClient: true})
+    .then(() => {
+
+        console.log("La conexión a la base de datos se ha realizado correctamente");
+        // CREAR EL SERVIDOR WEB CON NODEJS
+        app.listen(port, () => {
+            console.log("Servidor en ejecución");
+        });
+    })
+    .catch(err => console.log('ERROR: connecting to Database. ' + err));
+
+
