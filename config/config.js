1b98396243e8f200ea64f66b04848c047bb118ac
DenisCabezaV <maquia2796@gmail.com>
2019-08-16 03:23:26 -0500

    Actualización de puerto

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     url: 'mongodb://react:reactpass@alexkbza.com:27017/bdd-react-app',
-    serverport: 3000 
+    //serverport: 3000 puerto para desarrollo
+    serverport: 443 //puerto en producción
 }
